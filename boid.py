9873a7fb47101c826e3efa96eb63a17fdf1a012a
Halle Gordon-Jeary <hallegj@googlemail.com>
2023-02-08 23:52:02 +0000

    circles are on the page

---
+++ boid.py
@@ -0,0 +1,68 @@
+import tkinter as tk
+import numpy as np
+from numpy import random
+
+# basic boid class
+class Boid:
+    # position x,y
+    pos = np.array([0,0])
+    # acceleration x,y
+    acceleration = np.array([0,0])
+    # velocity x,y
+    velocity = np.array([10,10])
+    # speed = magnitude of velocity
+    speed = np.linalg.norm(velocity)
+    max_speed = 25
+
+    boid_radius = 25
+
+    def __init__(self, c) -> None:
+        self.canvas = c
+        self.c_width = c.winfo_width()
+        self.c_height = c.winfo_height()
+        print(self.c_width, self.c_height)
+
+        self.start_pos(self.c_width, self.c_height)
+        self.draw_boid(self.pos, self.boid_radius)
+
+
+    # random start position within starting area
+    # xMax, yMax bounds of start area
+    def start_pos(self, xMax, yMax):
+        self.pos = np.array([random.rand()*xMax, random.rand()*yMax])
+        print(self.pos)
+
+    def draw_boid(self, pos, rad):
+        self.drawing = self.canvas.create_oval(pos[0]-rad,pos[1]-rad,pos[0]+rad,pos[1]+rad, fill="black")
+
+    def calc_acceleration(self):
+        pass
+    # call the flocking funcs???
+
+    def calc_velocity(self):
+        # v + a
+        # keep new v under max spd
+        self.velocity = np.add(self.velocity, self.acceleration)
+        if self.speed > self.max_speed:
+            self.velocity[0] = (self.velocity[0] / self.speed) * self.max_speed
+            self.velocity[1] = (self.velocity[1] / self.speed) * self.max_speed
+
+
+    def move_boid(self):
+        self.calc_velocity
+
+        self.canvas.move(self, self.velocity[0], self.velocity[1])
+
+        (leftpos, toppos, rightpos, bottompos)=self.canvas.coords(self.drawing)
+   
+        if leftpos <=0 or rightpos>=self.c_width:
+            self.velocity[0]=-self.velocity[0]
+
+        if toppos <=0 or bottompos >=self.c_height:
+            self.velocity[1]=-self.velocity[1]
+        
+        self.canvas.after(30, self.move_boid)
+
+
+
+
--- canvas.py
+++ canvas.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+import numpy as np
+from boid import Boid
 
 # init window and canvas
 window = tk.Tk()
@@ -10,27 +12,44 @@ canvas_height = 720
 canvas_width = 1280
 canvas = tk.Canvas(window, bg="white", height=str(canvas_height), width=str(canvas_width))
 canvas.pack(pady=20)
+window.update()
+
+# draw boids
+num_of_boids = 5
+boids = []
+for n in range(num_of_boids):
+    # print(b)
+    boids.append(Boid(canvas))
+
+# move boids
+for boid in boids:
+    boid.move_boid()
+    canvas.after(30, boid.move_boid)
+
+window.update()
 
 # draw circle
-ball = canvas.create_oval(100,100,150,150, fill="red")
+# ball = canvas.create_oval(100,100,150,150, fill="red")
 
-# Move the ball
-xspeed=yspeed=3
+# # Move the ball
+# xspeed=yspeed=3
 
-def move_ball():
-   global xspeed, yspeed
+# def move_ball():
+#    global xspeed, yspeed
 
-   canvas.move(ball, xspeed, yspeed)
-   (leftpos, toppos, rightpos, bottompos)=canvas.coords(ball)
-   if leftpos <=0 or rightpos>=canvas_width:
-      xspeed=-xspeed
+#    canvas.move(ball, xspeed, yspeed)
+   
+#    (leftpos, toppos, rightpos, bottompos)=canvas.coords(ball)
+   
+#    if leftpos <=0 or rightpos>=canvas_width:
+#       xspeed=-xspeed
 
-   if toppos <=0 or bottompos >=canvas_height:
-      yspeed=-yspeed
+#    if toppos <=0 or bottompos >=canvas_height:
+#       yspeed=-yspeed
 
-   canvas.after(30,move_ball)
+#    canvas.after(30,move_ball)
 
-canvas.after(30, move_ball)
+# canvas.after(30, move_ball)
 
 # main loop
 window.mainloop()
