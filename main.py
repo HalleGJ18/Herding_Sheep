1b151c642cbafb7053193244fad74ae29b196a90
Halle Gordon-Jeary <hallegj@googlemail.com>
2023-03-01 20:19:17 +0000

    start laying out main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+import numpy as np
+from flock import Flock
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import pandas as pd
+
+# init window
+window = tk.Tk()
+window.title("Herding Sheep")
+window.geometry("1400x788-50-100")
+window.configure(background="grey")
+
+# generate sheep
+
+# plot sheep moving
+
+# main loop
+window.mainloop()
--- plot.py
+++ plot.py
@@ -10,21 +10,21 @@ root = tk.Tk()
 
 x = np.linspace(0, 10)
 
-figure3 = plt.Figure(figsize=(5, 4), dpi=100)
-ax3 = figure3.add_subplot(111)
-ax3.set_xlim([0, 10])
-scat = ax3.scatter(1,0)
-scatter3 = FigureCanvasTkAgg(figure3, root)
+fig = plt.Figure(figsize=(5, 4), dpi=100)
+ax = fig.add_subplot(111)
+ax.set_xlim([0, 10])
+scat = ax.scatter(1,0)
+scatter3 = FigureCanvasTkAgg(fig, root)
 scatter3.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
-ax3.legend(['index_price'])
-ax3.set_xlabel('Interest Rate')
-ax3.set_title('Interest Rate Vs. Index Price')
+ax.legend(['index_price'])
+ax.set_xlabel('Interest Rate')
+ax.set_title('Interest Rate Vs. Index Price')
 
 def animate(i):
     scat.set_offsets((x[i], 0))
     return scat,
 
-ani = animation.FuncAnimation(figure3, animate, repeat=True, frames=len(x) - 1, interval=50)
+ani = animation.FuncAnimation(fig, animate, repeat=True, frames=len(x) - 1, interval=50)
 
 # plt.show()
 
