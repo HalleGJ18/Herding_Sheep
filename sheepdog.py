5e79a96f441e345fb2ce87b9203120dd215e7d12
Halle Gordon-Jeary <hallegj@googlemail.com>
2023-03-06 19:03:40 +0000

    static sheepdog on plot

--- sheepdog.py
+++ sheepdog.py
@@ -2,3 +2,17 @@ import numpy as np
 from numpy import random
 import math
 from agent import Agent
+
+class Sheepdog(Agent):
+
+    target = np.array([600.0, 600.0])
+
+    vision_range = 300 # increase?
+
+    # calc flock density
+
+    # calc flock centre of mass
+
+    # calc line from target to flock com
+
+    
